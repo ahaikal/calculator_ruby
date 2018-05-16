28227c7e0e2032d85725c99ca48380a7073e2901
Alan haikal <alanhaikal@gmail.com>
2018-05-16 13:21:37 -0400

    calculator controller index and create

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,4 +1,21 @@
 class CalculatorController < ApplicationController
-  def calculate
+  def index
+  end
+
+  def create
+    @string = calculator_params[:string]
+    if @string.present?
+      @value = Operator.new.calculate(@string)
+      render :index
+    else
+      @notice = 'String is required'
+      render :index
+    end
+  end
+
+  private
+
+  def calculator_params
+    params.require(:calculator).permit(:string)
   end
 end
